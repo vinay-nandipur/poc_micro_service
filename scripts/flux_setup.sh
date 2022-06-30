ca0c20a7c81c5a7de712bdee975611e63d79bb66
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2022-06-30 15:32:47 +0530

    hide exports in stdout

--- scripts/flux_setup.sh
+++ scripts/flux_setup.sh
@@ -3,9 +3,11 @@
 # kube_cluster=$(kubectl config current-context)
 # if ! kubectl get ns flux ; then kubectl create ns flux ; fi
 
-export GITHUB_TOKEN=$(cat $HOME/.secrets/.git_token)
-export GITHUB_USER="vinay-nandipur"
-export GITHUB_REPO="flux2-kustomize-helm-example"
+{
+  export GITHUB_TOKEN=$(cat $HOME/.secrets/.git_token)
+  export GITHUB_USER="vinay-nandipur"
+  export GITHUB_REPO="flux2-kustomize-helm-example"
+} &> /dev/null 2>&1
 
 flux bootstrap github \
     --context=staging \
