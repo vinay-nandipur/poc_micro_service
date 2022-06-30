b40ba7bb6ccba28e12e102933aaa034e367b4cb6
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2022-06-30 13:48:34 +0530

    configured gh cli via workflow

---
+++ scripts/install_configure_gh_cli.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#install gh
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
+#upgrade gh
+sudo apt update
+sudo apt install gh
+
+#gh login
+gh auth login --with-token < $HOME/.secrets/.git_token > /dev/null 2>&1
