e0365362f7c1ab84129830ea2ff44053b6de13cd
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2022-06-30 13:54:28 +0530

    updated gh cli script

--- scripts/install_configure_gh_cli.sh
+++ scripts/install_configure_gh_cli.sh
@@ -5,11 +5,11 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
-sudo apt install gh
+sudo apt install gh -y
 
 #upgrade gh
 sudo apt update
-sudo apt install gh
+sudo apt install gh -y
 
 #gh login
 gh auth login --with-token < $HOME/.secrets/.git_token > /dev/null 2>&1
